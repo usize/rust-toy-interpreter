7c04f9602eb943d5cb0043b8add4b2f2f1cd46c5
Morgan Phillips <winter2718@gmail.com>
2016-01-17 18:38:16 -0600

    Add proper error handling in parser

--- src/parser.rs
+++ src/parser.rs
@@ -47,51 +47,51 @@ impl Parser {
         return Parser{lexer: Lexer::from(tokens)};
     }
 
-    fn parse_term(&mut self) -> Expr {
+    fn parse_term(&mut self) -> Result<Expr, String> {
         match *self.lexer.curr_type() {
             TokenType::Int => {
                 let int = self.lexer.curr_value().parse::<i32>().unwrap();
-                return Expr::Atom(Value::Int(int));
+                return Ok(Expr::Atom(Value::Int(int)));
             },
             TokenType::Float => {
                 let float = self.lexer.curr_value().parse::<f32>().unwrap();
-                return Expr::Atom(Value::Float(float));
+                return Ok(Expr::Atom(Value::Float(float)));
             },
             TokenType::Str => {
-                return Expr::Atom(Value::Str(self.lexer.curr_value()));
+                return Ok(Expr::Atom(Value::Str(self.lexer.curr_value())));
             },
             TokenType::Identifier => {
                 let e1 = Expr::GetName(self.lexer.curr_value());
                 if self.lexer.next_token() {
                     match *self.lexer.curr_type() {
-                        TokenType::BinOp => return self.parse_binop(e1),
+                        TokenType::BinOp => return Ok(try!(self.parse_binop(e1))),
                         TokenType::LPar  => {
                             let mut expr_stack = Vec::new();
                             expr_stack.push(e1);
                             self.lexer.next_token();
                             while !self.lexer.current_is_type(TokenType::RPar) {
-                                expr_stack.push(self.parse_expression());
+                                expr_stack.push(try!(self.parse_expression()));
                                 self.lexer.next_token();
                                 if self.lexer.current_is_type(TokenType::Comma) {
                                     self.lexer.next_token();
                                 }
                             }
-                            return Expr::Call(expr_stack);
+                            return Ok(Expr::Call(expr_stack));
                         },
-                        _ => return e1
+                        _ => return Ok(e1)
                     }
                 }
-                return e1;
+                return Ok(e1);
             },
             TokenType::LPar => {
                 self.lexer.next_token();
                 let e = self.parse_expression();
-                self.lexer.match_token(TokenType::RPar).unwrap();
+                try!(self.lexer.match_token(TokenType::RPar));
                 return e;
             },
             TokenType::Function => {
                 self.lexer.next_token();
-                self.lexer.match_token(TokenType::LPar).unwrap();
+                try!(self.lexer.match_token(TokenType::LPar));
                 self.lexer.next_token();
                 let mut args = Vec::new();
                 while self.lexer.current_is_type(TokenType::Identifier) {
@@ -101,52 +101,56 @@ impl Parser {
                          self.lexer.next_token();
                      }
                 }
-                self.lexer.match_token(TokenType::RPar).unwrap();
+                try!(self.lexer.match_token(TokenType::RPar));
                 self.lexer.next_token();
-                let body = self.parse_block();
+                let body = try!(self.parse_block());
                 self.lexer.next_token();
-                return Expr::Function(args, body);
+                return Ok(Expr::Function(args, body));
             },
-            _ => Expr::Nil
+            _ => Ok(Expr::Nil)
         }
     }
 
     // TODO: precedences ~!!@
-    fn parse_binop(&mut self, e1: Expr) -> Expr {
+    fn parse_binop(&mut self, e1: Expr) -> Result<Expr, String> {
         let (op, _) = Lexer::bin_op(&self.lexer.curr_value()[..]).unwrap();
         self.lexer.next_token();
-        let bop = BinaryOp{l_expr: e1, op: op, r_expr: self.parse_expression()};
-        return Expr::BinaryOperation(Box::new(bop));
+        let r_expr = try!(self.parse_expression());
+        let bop = BinaryOp{l_expr: e1, op: op, r_expr: r_expr};
+        return Ok(Expr::BinaryOperation(Box::new(bop)));
     }
 
     // Wrap in a while loop
-    fn parse_expression(&mut self) -> Expr {
-        let e1 = self.parse_term();
+    fn parse_expression(&mut self) -> Result<Expr, String> {
+        let e1 = try!(self.parse_term());
         if self.lexer.tokens_remaining() > 0 &&
            self.lexer.current_is_type(TokenType::BinOp) {
             return self.parse_binop(e1);
         }
-        return e1;
+        return Ok(e1);
     }
 
-    fn parse_statement(&mut self) -> Statement {
+    fn parse_statement(&mut self) -> Result<Statement, String> {
         match *self.lexer.curr_type() {
             TokenType::Let => {
                self.lexer.next_token();
-               self.lexer.match_token(TokenType::Identifier).unwrap();
+               try!(self.lexer.match_token(TokenType::Identifier));
                let name = self.lexer.curr_value();
                self.lexer.next_token();
                self.lexer.match_token(TokenType::Equals).unwrap();
                self.lexer.next_token();
-               let e = self.parse_expression();
-               return Statement::Assignment(DefLet{name: name, expr: e});
+               let e = self.parse_expression().unwrap();
+               return Ok(Statement::Assignment(DefLet{name: name, expr: e}));
             },
-            _ => return Statement::Expression(self.parse_expression()),
+            _ => {
+                let e = try!(self.parse_expression());
+                return Ok(Statement::Expression(e));
+            },
         }
     }
 
-    fn parse_block(&mut self) -> Vec<Statement> {
-        self.lexer.match_token(TokenType::LCBrace).unwrap();
+    fn parse_block(&mut self) -> Result<Vec<Statement>, String> {
+        try!(self.lexer.match_token(TokenType::LCBrace));
         let mut block_tokens = Vec::new();
         let mut brace_count = 1;
         while self.lexer.next_token() {
@@ -161,37 +165,34 @@ impl Parser {
             }
             block_tokens.push(self.lexer.curr_token().clone());
         }
-        self.lexer.match_token(TokenType::RCBrace).unwrap();
+        try!(self.lexer.match_token(TokenType::RCBrace));
         return Parser::parse_from_tokens(block_tokens);
     }
 
-    fn parse_from_tokens(tokens: Vec<Token>) -> Vec<Statement> {
+    fn parse_from_tokens(tokens: Vec<Token>) -> Result<Vec<Statement>, String> {
         let mut block_parser = Parser::from(tokens);
         return block_parser.parse_program();
     }
 
-    pub fn parse_program(&mut self) -> Vec<Statement> {
+    pub fn parse_program(&mut self) -> Result<Vec<Statement>, String> {
         println!("Lexer: {:?}", self.lexer.tokens());
         let mut program = Vec::new();
         if self.lexer.tokens().len() > 0 {
             loop {
-                program.push(self.parse_statement());
+                program.push(try!(self.parse_statement()));
                 if self.lexer.tokens_remaining() < 1 {
-                   return program;
+                   return Ok(program);
                 }
                 self.lexer.next_token();
             }
         }
-        return program;
+        return Ok(program);
     }
 
     pub fn parse_lines(&mut self, text: String) -> Result<Vec<Statement>, String> {
-        match self.lexer.tokenize(text.clone()) {
-            Err(msg) => return Err(msg),
-            _ => ()
-        }
+        try!(self.lexer.tokenize(text.clone()));
         let program = self.parse_program();
         self.lexer.reset();
-        return Ok(program);
+        return program;
     }
 }
