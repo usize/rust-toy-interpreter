3e87c8d9aa5f44d4490645b39de87d32278d6bcc
Morgan Phillips <winter2718@gmail.com>
2016-01-22 01:18:08 -0600

    Remove parser/vm messages

--- src/main.rs
+++ src/main.rs
@@ -52,11 +52,11 @@ fn main() {
         match parser.parse_lines(input.clone()) {
             Err(msg) => println!("{}", msg),
             Ok(statements) => {
-                println!("Parser: \n\t{:?}", &statements);
+                //println!("Parser: \n\t{:?}", &statements);
                 let script = compile_script(statements);
                 vm.load(script);
                 let result = vm.run(&mut scopes);
-                println!("VM: \n\tstack: {:?}, \n\tprogram: {:?}\n", vm.stack(), vm.program());
+                //println!("VM: \n\tstack: {:?}, \n\tprogram: {:?}\n", vm.stack(), vm.program());
                 match result {
                     Ok(Some(value)) => println!("{:?}", value),
                     Ok(None) => (),
